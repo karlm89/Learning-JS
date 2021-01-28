cb5e9c962ba92f68e1e653c07bed3d51891b6331
Karl Murray <karlm89@me.com>
2021-01-27 23:27:55 -0600

    add the 2nd way our_first_conditional_exercise.js

--- js/bootcamp_2021/object_access_exercise.js
+++ js/bootcamp_2021/object_access_exercise.js
@@ -1,13 +1,12 @@
 //PLEASE DON'T TOUCH THIS LINE!
 const restaurant = {
-    name: 'Ichiran Ramen',
-    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
-    city: 'Brooklyn',
-    state: 'NY',
-    zipcode: '11206',
+	name: 'Ichiran Ramen',
+	address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+	city: 'Brooklyn',
+	state: 'NY',
+	zipcode: '11206',
 }
 //YOUR CODE GOES DOWN HERE:
+
 // let fullAddress = restaurant.address + ', ' + restaurant.city + ', ' + restaurant.state + ' ' + restaurant.zipcode
-
 let fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}`
-
