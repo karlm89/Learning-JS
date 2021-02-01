6b9ec6a35bdb4d4e165683f520de1ada59ade90d
Karl Murray <karlm89@me.com>
2021-02-01 08:54:52 -0600

    add 4_for_of_practice.js

---
+++ js/section-19/4_for_of_practice.js
@@ -0,0 +1,11 @@
+/*
+    What you need to know :
+        for...of - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
+*/
+
+const numbers = [1,2,3,4,5,6,7,8,9]; //DON'T CHANGE THIS LINE PLEASE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for(let number of numbers) {
+    console.log(number*number)
+}
