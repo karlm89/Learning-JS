f7673b9fd131678d0db3c87310d6bd1f4d3853e5
Karl Murray <karlm89@me.com>
2021-02-09 01:17:41 -0600

    add 3_multiple_args_exercise.js

---
+++ js/section-20/3_multiple_args_exercise.js
@@ -0,0 +1,14 @@
+/*
+    What you need to know :
+        Function - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function    
+*/
+
+// define isSnakeEyes below
+
+function isSnakeEyes(die1, die2) {
+  if (die1 === 1 && die2 === 1) {
+    console.log('Snake Eyes!')
+  } else {
+    console.log('Not Snake Eyes!')
+  }
+}
