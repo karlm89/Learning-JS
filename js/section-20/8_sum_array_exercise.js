a5f36b7aca42c4c9db133a2cc45950d5b7bb7e01
Karl Murray <karlm89@me.com>
2021-02-09 03:55:48 -0600

    add 8 and 1

--- js/section-20/8_sum_array_exercise.js
+++ js/section-20/8_sum_array_exercise.js
@@ -1,5 +1,15 @@
 // DEFINE YOUR FUNCTION BELOW:
 
+//Function Statement
+function sumArray(array) {
+  let sum = 0
+  for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i]
+  }
+  return sum
+}
+
+// Arrow Function aka Function Expression
 const sumArray = (array) => {
   let sum = 0
   for (let i = 0; i < array.length; i++) {
