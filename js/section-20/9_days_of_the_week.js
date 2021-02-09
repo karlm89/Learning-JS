f2746a85b6f6d886deed02d92f62b2abe6c8de8a
Karl Murray <karlm89@me.com>
2021-02-09 02:57:08 -0600

    add 8 and 9

---
+++ js/section-20/9_days_of_the_week.js
@@ -0,0 +1,31 @@
+// DEFINE YOUR FUNCTION BELOW:
+
+// God, I hate switch statements... Also, unrealated, everyone knows 'Sunday' is the first day of the week.
+const returnDay = (day) => {
+  switch (day) {
+    case 1:
+      return 'Monday'
+      break
+    case 2:
+      return 'Tuesday'
+      break
+    case 3:
+      return 'Wednesday'
+      break
+    case 4:
+      return 'Thursday'
+      break
+    case 5:
+      return 'Friday'
+      break
+    case 6:
+      return 'Saturday'
+      break
+    case 7:
+      return 'Sunday'
+      break
+    // Return Null as default...
+    default:
+      return null
+  }
+}
