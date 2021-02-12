8fcd95053eca7b036527edb4a44249e0ec6d8361
Karl Murray <karlm89@me.com>
2021-02-12 04:57:43 -0600

    added Obj version of days_of_the_week.js

--- js/section-20/9_days_of_the_week.js
+++ js/section-20/9_days_of_the_week.js
@@ -32,9 +32,29 @@ const returnDay = (day) => {
 
 // Shorter clearner way, and you don't have to use a switch statement.
 const returnDay = (day) => {
-  let dayArray = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  if (day >= 1 && day <= days.length) {
+    return days[day - 1]
+  } else {
+    return null
+  }
+}
+
+// Object Version :
+
+let daysObj = {
+  1: 'Monday',
+  2: 'Tuesday',
+  3: 'Wednesday',
+  4: 'Thursday',
+  5: 'Friday',
+  6: 'Saturday',
+  7: 'Sunday',
+}
+
+const returnDay = (day) => {
   if (day >= 1 && day <= 7) {
-    return dayArray[day - 1]
+    return daysObj[day]
   } else {
     return null
   }
